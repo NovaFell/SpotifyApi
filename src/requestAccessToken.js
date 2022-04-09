2fca41982b78b0886036115e767197a78c3610fc
NovaFell <60708235+NovaFell@users.noreply.github.com>
2022-04-09 17:32:59 -0500

    Create requestAccessToken.js

---
+++ src/requestAccessToken.js
@@ -0,0 +1,26 @@
+app.get('/callback', function(req, res) {
+  
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  
+  if (state === null) {
+    res.redirect('/#' +
+                 querystring.stringify({
+                    error: 'state_mismatch'
+    }));
+  } else {
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code : code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization _code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' +
+                                                client_secret).toString('base64'))
+      },
+      json: true
+    };
+  }
+});
